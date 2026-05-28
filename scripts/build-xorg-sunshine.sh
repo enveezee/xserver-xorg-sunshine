eeb26226e7f7faf9d19f2c1c79ca44d2dd33b935
enveezee <enveezee@gmail.com>
2026-05-28 13:55:35 -0400

    cleanup build script

--- scripts/build-xorg-sunshine.sh
+++ scripts/build-xorg-sunshine.sh
@@ -2,7 +2,13 @@
 set -e
 
 # Locate source dir
-PKGDIR=$(find . -maxdepth 1 -type d ( -name "xorg-server-*" -o -name "xserver-xorg-core-*" ) | head -n1)
+PKGDIR=$(\
+    find . -maxdepth 1 -type d\ 
+    (\
+        -name "xorg-server-*" -o\
+        -name "xserver-xorg-core-*"\
+    )\
+    | head -n1 )
 if [ -z "$PKGDIR" ]; then
     echo "Source dir not found."
     exit 1
@@ -13,12 +19,6 @@ cp -r debian/* "$PKGDIR/debian/"
 
 # Enter the source dir
 cd "$PKGDIR"
-
-# Manage permissions of added files
-chmod 755 debian/xserver-xorg-sunshine/usr/bin/Xorg-sunshine
-chmod 755 debian/xserver-xorg-sunshine/usr/local/bin/update-sunshine-xorg
-chmod 755 debian/xserver-xorg-sunshine/usr/local/sbin/xserver-xorg-sunshine-update
-chmod 644 debian/xserver-xorg-sunshine/lib/systemd/system/xserver-xorg-sunshine-update.service
 
 # Build the renamed core package
 dpkg-buildpackage -b -d -us -uc -Pnocheck
