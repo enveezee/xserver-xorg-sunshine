57ab180e8c8b216ec59649a7dc00d83d42879186
nvz <72872161+enveezee@users.noreply.github.com>
2026-09-02 07:02:07 -0400

    Change debug output to show specific debian files
    Updated debug output to display specific debian files instead of listing the entire debian directory.

--- scripts/build-xorg-sunshine.sh
+++ scripts/build-xorg-sunshine.sh
@@ -16,7 +16,7 @@ cp -r debian/* "$PKGDIR/debian/"
 cd "$PKGDIR"
 pwd
 echo "DEBUG: build proceeding as planned, if you don't see this message, this script died."
-ls -lR "debian/"
+for f in debian/control debian/xserver-xorg-sunshine.install; do cat "$f"; echo "#####"; done
 # Build the renamed core package
 dpkg-buildpackage -b -d -us -uc -Pnocheck
 
